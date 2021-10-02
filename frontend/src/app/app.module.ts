ec04806743c37bf8e68f64eff042b2fc6e1138f7
ksamorodov <ksamorodov@yandex.ru>
2021-10-02 08:45:05 +0300

    delete unused module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, MainPageComponent ],
+    declarations: [ AppComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
